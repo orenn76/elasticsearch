13f38287764c4800d4179663aef07c3defaf005d
Oren Ninyo <oren76@gmail.com>
2023-11-21 18:04:58 +0200

    First commit

---
+++ src/main/java/com/ninyo/elasticsearch/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ninyo.elasticsearch.repositories;
+
+import com.ninyo.elasticsearch.models.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ElasticsearchRepository<Product, String> {
+}
